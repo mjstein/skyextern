48512c3d8582b33a492969ce940a92087381765a
Your Name <you@example.com>
2016-06-23 19:25:31 +0100

    trial dhcp-dns binding

--- modules/profiles/manifests/dhcpserver.pp
+++ modules/profiles/manifests/dhcpserver.pp
@@ -4,20 +4,24 @@ class profiles::dhcpserver(
 {
   class { 'dhcp':
     nameservers  => $nameservers,
+    dnsdomain    => ['blajh.com', '10.0.2.in-addr.arpa'],
+    dnsupdatekey => "/tmp/key",
     interfaces   => ['eth0'],
     pxeserver    => $networking['ip'],
     pxefilename  => 'pxelinux.0',
   }
+  create_resources(dhcp::pool, hiera_hash('dhcp::pool'))
+  create_resources(dhcp::host, hiera_hash('dhcp::host'))
 
-  dhcp::pool{ 'ops.dc1.example.net':
-    network               => '10.0.2.0',
-    mask                => '255.255.255.0',
-    range             => '10.0.2.100 10.0.2.200',
-    gateway         => '10.0.2.1',
-  }
-  dhcp::host {
-    'server1': mac     => "00:50:56:00:00:01", ip => "10.0.2.51";
-    'server2': mac   => "00:50:56:00:00:02", ip => "10.0.2.52";
-    'server3': mac => "00:50:56:00:00:03", ip => "10.0.2.53";
-  }
+  #    dhcp::pool{ 'ops.dc1.example.net':
+  #    network               => '10.0.2.0',
+  #    mask                => '255.255.255.0',
+  #    range             => '10.0.2.100 10.0.2.200',
+  #    gateway         => '10.0.2.1',
+  #  }
+  #  dhcp::host {
+  #    'server1': mac     => "00:50:56:00:00:01", ip => "10.0.2.51";
+  #    'server2': mac   => "00:50:56:00:00:02", ip => "10.0.2.52";
+  #    'server3': mac => "00:50:56:00:00:03", ip => "10.0.2.53";
+  #  }
 }
