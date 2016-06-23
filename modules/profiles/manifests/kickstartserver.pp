d3ea38743e6a8f9f8b70b2dfd5bfe518db8e164b
Your Name <you@example.com>
2016-06-23 14:27:14 +0100

    add in xinetd

--- modules/profiles/manifests/kickstartserver.pp
+++ modules/profiles/manifests/kickstartserver.pp
@@ -1,3 +1,20 @@
 class profiles::kickstartserver{
-include vsftp
+  # install vsftp wrapper for ftp
+
+# install tftp and syslinux
+xinetd::service { 'tftp':
+  port        => '69',
+  server      => '/usr/sbin/in.tftpd',
+  server_args => '-s /var/lib/tftpboot',
+  socket_type => 'dgram',
+  protocol    => 'udp',
+  cps         => '100 2',
+  flags       => 'IPv4',
+  per_source  => '11',
+}
+['tftp-server', 'syslinux'].each |$package| {
+  package{$package:
+    ensure => latest
+  }
+}
 }
