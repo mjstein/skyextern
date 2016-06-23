fbeaf113e3e5d103b3368662bc137c4fe1326022
Your Name <you@example.com>
2016-06-23 13:55:28 +0100

    update

--- install_puppet.sh
+++ install_puppet.sh
@@ -1,4 +1,4 @@
-rpm -ivh https://yum.puppetlabs.com/el/7/products/x86_64/puppetlabs-release-7-11.noarch.rpm
-yum install -y puppet-server
+sudo rpm -Uvh https://yum.puppetlabs.com/puppetlabs-release-pc1-el-7.noarch.rpm
+yum install -y puppetserver vim
 ln -s /opt/puppetlabs/bin/puppet /usr/bin/puppet
 
